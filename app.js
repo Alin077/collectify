dd716812122b0ab58e5bca5c243f4f82ffce8ef2
Alin <alinduwal984@gmail.com>
2026-05-31 22:18:52 +0545

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+let items = [
+  {
+    name: "1962 Rookie Card",
+    category: "Cards",
+    rarity: "Legendary",
+    price: 8650,
+    seller: "Ava",
+    rating: 4.9,
+    time: 90,
+    image: ""
+  },
+  {
+    name: "Astro Knight Comic",
+    category: "Comics",
+    rarity: "Epic",
+    price: 1200,
+    seller: "Mira",
+    rating: 4.7,
+    time: 240,
+    image: ""
+  },
+  {
+    name: "Old Silver Coin",
+    category: "Coins",
+    rarity: "Rare",
+    price: 450,
+    seller: "Theo",
+    rating: 4.6,
+    time: 160,
+    image: ""
+  }
+];
+
